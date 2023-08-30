babf721a8178a77dee2679d13eabc76f3ae2b064
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-30 19:47:23 -0300

    Create aula4.py

---
+++ python/aulas/aula4.py
@@ -0,0 +1,14 @@
+def calcMedia(valor1, valor2):
+    return (valor1 + valor2) / 2
+
+nome = 'Ana'
+nota1 = 8.3
+nota2 = 6.4
+
+print(f'{nome} sua média é {calcMedia(nota1, nota2)}')
+
+nome = "Isabella"
+nota1 = 7.9
+nota2 = 9.6
+
+print(f'{nome} sua média é {calcMedia(nota1, nota2)}')
