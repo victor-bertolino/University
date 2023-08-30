4c455db53f6fb40dff4744d8be45c24eb4a99d2a
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-08-29 21:44:36 -0300

    Create tabuada.js

---
+++ javascript/aulas/tabuada.js
@@ -0,0 +1,7 @@
+let multiplicador = 4;
+let resultado = 0;
+
+for (let i = 0; i <= 10; i++) {
+    resultado = multiplicador * i;
+    console.log(multiplicador + " X " + i + " = " + resultado);    
+}
