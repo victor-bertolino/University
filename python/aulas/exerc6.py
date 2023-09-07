51ce094075f73e63361bf84845f53f8d19f3087e
Victor Bertolino <victorbertolino.pro@gmail.com>
2023-09-06 21:53:34 -0300

    Update exerc6.py

---
+++ python/aulas/exerc6.py
@@ -0,0 +1,6 @@
+lista = [12,54.6,6765,34,2.5]
+
+print(f"Maior valor da lista {max(lista)}")
+print(f"Menor valor da lista {min(lista)}")
+print(f"Média de valores {sum(lista) / len(lista)}")
+print(f"Soma dos valores da lista {sum(lista)}")
